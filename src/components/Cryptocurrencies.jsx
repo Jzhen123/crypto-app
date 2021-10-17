3be75ce447b0752c312ec2c5995ea93238ca4e09
Jzhen123 <jiayuzheng01@gmail.com>
2021-10-17 13:53:25 -0400

    Added a search bar that conditionally renders oso it doesn't appear on the Home Page where it shows just the top 10.

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,19 +1,32 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import millify from 'millify';
 import { Link } from 'react-router-dom';
 import { Card, Row, Col, Input } from 'antd';
 
 import { useGetCryptosQuery } from '../services/cryptoApi';
 
-const Cryptocurrencies = () => {
-    const { data: cryptosLists, isFetching } = useGetCryptosQuery();
-    const [cryptos, setCryptos] = useState(cryptosLists?.data?.coins)
+const Cryptocurrencies = ({ simplified }) => {
+    const count = simplified ? 10 : 100;
+    const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
+    const [cryptos, setCryptos] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
 
+    useEffect(() => {
+        const filteredData = cryptosList?.data?.coins.filter((coin) => coin.name.toLowerCase().includes(searchTerm.toLocaleLowerCase()));
+        setCryptos(filteredData);
+    }, [cryptosList, searchTerm])
+
+    if (isFetching) return 'Loading...';
     console.log(cryptos)
     return (
         <>
+            {!simplified && (
+                <div className="search-dash-crypto">
+                    <Input placeholder="Search Cryptocurrencry" onChange={(e) => setSearchTerm(e.target.value)} />
+                </div>
+            )}
             <Row gutter={[32, 32]} className="crypto-card-container">
-                {cryptos.map((currency) => (
+                {cryptos?.map((currency) => (
                     <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
                         <Link to={`crypto/${currency.id}`}>
                             <Card
